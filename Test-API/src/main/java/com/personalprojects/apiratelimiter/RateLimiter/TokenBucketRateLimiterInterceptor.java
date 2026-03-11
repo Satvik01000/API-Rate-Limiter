50ff768fe15e953ed9ab97450e25cd3c593eb8ce
Satvik Mishra <satvikmishra2192@gmail.com>
2026-03-11 10:20:42 +0530

    Token Bucket Rate Limiter Complete

---
+++ Test-API/src/main/java/com/personalprojects/apiratelimiter/RateLimiter/TokenBucketRateLimiterInterceptor.java
@@ -0,0 +1,27 @@
+package com.personalprojects.apiratelimiter.RateLimiter;
+
+import com.PersonalProjects.TokenBucketRateLimiter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class TokenBucketRateLimiterInterceptor implements HandlerInterceptor {
+    private final TokenBucketRateLimiter rateLimiter = new TokenBucketRateLimiter(10, 1);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String ipAddress = request.getRemoteAddr();
+        boolean isAllowed = rateLimiter.allowRequest(ipAddress);
+
+        if(isAllowed) {
+            return  true;
+        } else {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            response.getWriter().write("429 - Too Many Requests. Try again later.");
+            return false;
+        }
+    }
+}
